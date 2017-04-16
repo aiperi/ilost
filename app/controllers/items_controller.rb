d6a1f953485c6bb7f40d325c9eab2e73076c9c16
Aiperi <aiperi.akunova@gmail.com>
2017-04-16 17:34:03 +0300

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,59 @@
+class ItemsController < ApplicationController
+	before_action :find_item, only: [:show, :edit, :update, :destroy]	
+
+	def index
+		@items = Item.all.order("created_at DESC")
+	end
+
+	def show 
+	
+	end
+
+	def new
+		@item = current_user.items.build
+		@categories = Category.all.map { |c| [c.name, c.id] }
+	end
+
+	def create
+		@item = current_user.items.build(item_params)
+		@item.category_id = params[:category_id]
+
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@categories = Category.all.map { |c| [c.name, c.id] }
+
+	end
+
+	def update
+		
+		if @item.update(item_params)
+			redirect_to item_path(@item)
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to root_path
+
+	end
+
+	private 
+
+		def item_params
+			params.require(:item).permit(:title, :description, :author, :category_id)
+		
+		end
+
+		def find_item
+			@item = Item.find(params[:id])
+
+		end
+end
